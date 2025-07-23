8f9bb12ad495cd764f61ab60daf028e470daaa10
hWataame <151874689+HWataame@users.noreply.github.com>
2025-07-23 13:18:51 +0900

    feat: アセンブリ情報を追加

---
+++ Scripts/AssembltInfo/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+/*
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+アセンブリ情報を保持するファイル
+
+AssemblyInfo.cs
+────────────────────────────────────────
+バージョン: 1.0.0
+2025 Wataame(HWataame)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+*/
+using System.Reflection;
+
+[assembly: AssemblyTitle("LibEnumGenerics for Unity")]
+[assembly: AssemblyProduct("LibEnumGenerics for Unity")]
+[assembly: AssemblyCompany("Wataame(HWataame)")]
+[assembly: AssemblyCopyright("2025 Wataame(HWataame)")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("250609_1222")]
